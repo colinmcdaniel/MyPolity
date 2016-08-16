b077a7cd2cbddc20138867591c4d41ab8c6a2d9d
Spencer Charest <spencercharest@Spencers-MacBook-Pro.local>
2016-08-16 09:27:01 -0700

    added example slideshow with slick.js

--- assets/javascript/openStates.js
+++ assets/javascript/openStates.js
@@ -1,3 +1,11 @@
+$(document).ready(function(){
+      $('.slides').slick({
+        arrows: true,
+        dots: true,
+        slidesToShow: 2,
+        infinite: true
+      });
+    });
 $(document).on('click', '#submit-button', function() {
   var user = {
     zip: ('#zip').val()
