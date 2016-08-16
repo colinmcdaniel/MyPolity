65a1ecdc79bcacf8f68a9aa38685dcdf548d182f
Jemma Marie <JemmaMarie@iMacs-iMac-9.local>
2016-08-15 21:40:18 -0700

    update openState changes

--- assets/javascript/openStates.js
+++ assets/javascript/openStates.js
@@ -1,11 +1,17 @@
 
 
-var siteURL = "http://openstates.org/api/v1/"
-var APIkey = "f58d2e11ccbe4471bdb7485c4fee0058"
+// var siteURL = "http://openstates.org/api/v1/"
+// var APIkey = "f58d2e11ccbe4471bdb7485c4fee0058"
 
-$(document).on('click', '#submit-button', function() {
-    var topic = 'metadata/ca';
-    var queryURL = siteURL + topic + "/?" + "&apikey=" + APIkey;
+
+$(document).on('click', '#search_button', function() {
+    // var topic = 'metadata/ca'; + topic + "/?" +
+    var geoKey = "AIzaSyAt-c78Z1cXHLrKs0IyHrq6s0xEkrF1lwQ";
+    var zip = $('#zipInput').val();
+    zip = '91344';
+    var geoLocationURL ="https://maps.googleapis.com/maps/api/geocode/json?address="+zip+"&key=";
+
+    var queryURL = geoLocationURL + geoKey;
     console.log(queryURL);
     $.ajax({
             url: queryURL,
@@ -15,3 +21,4 @@ $(document).on('click', '#submit-button', function() {
             console.log(response);
         });
 });
+
