c7f564904c9ea4d87f4d7c33c2813b6b9f6acda7
Spencer Charest <spencercharest@Spencers-Mac-Mini.local>
2016-08-17 15:11:07 -0700

    added some placeholders to make slick responsive

--- assets/javascript/openStates.js
+++ assets/javascript/openStates.js
@@ -1,9 +1,36 @@
 $(document).ready(function(){
       $('.slides').slick({
-        arrows: false,
+        arrows: true,
         dots: true,
         slidesToShow: 2,
         infinite: true,
+        responsive: [
+    // {
+    //   breakpoint: 1024,
+    //   settings: {
+    //     slidesToShow: 3,
+    //     slidesToScroll: 3,
+    //     infinite: true,
+    //     dots: true
+    //   }
+    // },
+    // {
+    //   breakpoint: 600,
+    //   settings: {
+    //     slidesToShow: 2,
+    //     slidesToScroll: 2
+    //   }
+    // },
+    {
+      breakpoint: 376,
+      settings: {
+        arrows: false,
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
       });
     });
 $(document).on('click', '#submit-button', function() {
