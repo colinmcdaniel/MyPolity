e231e58df2ea97593875c3338b30a0156b0c78e7
Jemma Marie <JemmaMarie@iMacs-iMac-9.local>
2016-08-17 22:35:00 -0700

    pull most recent changes

--- assets/javascript/openStates.js
+++ assets/javascript/openStates.js
@@ -4,21 +4,20 @@
 // var APIkey = "f58d2e11ccbe4471bdb7485c4fee0058"
 
 
-$(document).on('click', '#search_button', function() {
-    // var topic = 'metadata/ca'; + topic + "/?" +
-    var geoKey = "AIzaSyAt-c78Z1cXHLrKs0IyHrq6s0xEkrF1lwQ";
-    var zip = $('#zipInput').val();
-    zip = '91344';
-    var geoLocationURL ="https://maps.googleapis.com/maps/api/geocode/json?address="+zip+"&key=";
+// $(document).on('click', '#search_button', function() {
+//     // var topic = 'metadata/ca'; + topic + "/?" +
+//     var geoKey = "AIzaSyAt-c78Z1cXHLrKs0IyHrq6s0xEkrF1lwQ";
+//     var zip = $('#zipInput').val();
+//     var geoLocationURL ="https://maps.googleapis.com/maps/api/geocode/json?address="+zip+"&key=";
 
-    var queryURL = geoLocationURL + geoKey;
-    console.log(queryURL);
-    $.ajax({
-            url: queryURL,
-            method: 'GET'
-        })
-        .done(function(response) {
-            console.log(response);
-        });
-});
+//     var queryURL = geoLocationURL + geoKey;
+//     console.log(queryURL);
+//     $.ajax({
+//             url: queryURL,
+//             method: 'GET'
+//         })
+//         .done(function(response) {
+//             console.log(response);
+//         });
+// });
 
