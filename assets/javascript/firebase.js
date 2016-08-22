79127aca94fe8df8eef7d897fef513e889e14012
Jackemuk <jackemuk@DevBook.local>
2016-08-22 13:48:06 -0700

    Cleaned up the firebase code from openStates and moved to firebase.js

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -1,24 +1,28 @@
-// var Db;
 
-//   // Initialize Firebase
-// $(document).ready(function(){
-//   var config = {
-//     apiKey: "AIzaSyDo0YPqvSLALkV93436vn8Qj8s1AoBBmow",
-//     authDomain: "mypolity-d8c63.firebaseapp.com",
-//     databaseURL: "https://mypolity-d8c63.firebaseio.com",
-//     storageBucket: "mypolity-d8c63.appspot.com",
-//   };
-//   firebase.initializeApp(config);
-//   Db = firebase.database();
+// Colin's Firebase
+  // var fff = {
+  //   apiKey: "AIzaSyB8TqG5FuYXornV5pOfov-IRbGJH3y-epw",
+  //   authDomain: "mypolity-temp.firebaseapp.com",
+  //   databaseURL: "https://mypolity-temp.firebaseio.com",
+  //   storageBucket: "mypolity-temp.appspot.com",
+  // };
 
+// Spencer's Firebase
+// var config = {
+// apiKey: "AIzaSyA6P8YWzzxROrGRStOxa1kEFbDau5SVzW8",
+// authDomain: "mypolity-4808b.firebaseapp.com",
+// databaseURL: "https://mypolity-4808b.firebaseio.com",
+// storageBucket: "mypolity-4808b.appspot.com",
+// };
+
+// Gary's firebase
 //
-// });
+// var config = {
+//   apiKey: "AIzaSyDo0YPqvSLALkV93436vn8Qj8s1AoBBmow",
+//   authDomain: "mypolity-d8c63.firebaseapp.com",
+//   databaseURL: "https://mypolity-d8c63.firebaseio.com",
+//   storageBucket: "mypolity-d8c63.appspot.com",
+// };
 
-var config = {
-apiKey: "AIzaSyA6P8YWzzxROrGRStOxa1kEFbDau5SVzW8",
-authDomain: "mypolity-4808b.firebaseapp.com",
-databaseURL: "https://mypolity-4808b.firebaseio.com",
-storageBucket: "mypolity-4808b.appspot.com",
-};
 
 firebase.initializeApp(config);
--- assets/javascript/openStates.js
+++ assets/javascript/openStates.js
@@ -1,36 +1,3 @@
-// Colin's Firebase
-  // var fff = {
-  //   apiKey: "AIzaSyB8TqG5FuYXornV5pOfov-IRbGJH3y-epw",
-  //   authDomain: "mypolity-temp.firebaseapp.com",
-  //   databaseURL: "https://mypolity-temp.firebaseio.com",
-  //   storageBucket: "mypolity-temp.appspot.com",
-  // };
-  // firebase.initializeApp(fff);
-
-
-// Spencer's Firebase
-// var config = {
-// apiKey: "AIzaSyA6P8YWzzxROrGRStOxa1kEFbDau5SVzW8",
-// authDomain: "mypolity-4808b.firebaseapp.com",
-// databaseURL: "https://mypolity-4808b.firebaseio.com",
-// storageBucket: "mypolity-4808b.appspot.com",
-// };
-
-// firebase.initializeApp(config);
-
-// Division 3 gives you US information
-// getRepresentatives("10824 Lindbrook Drive","Los Angeles","California","90024",3);
-
-// Gary firebase
-//
-// var config = {
-//   apiKey: "AIzaSyDo0YPqvSLALkV93436vn8Qj8s1AoBBmow",
-//   authDomain: "mypolity-d8c63.firebaseapp.com",
-//   databaseURL: "https://mypolity-d8c63.firebaseio.com",
-//   storageBucket: "mypolity-d8c63.appspot.com",
-// };
-//
-// firebase.initializeApp(config);
 
 var openStatesURL = "http://openstates.org/api/v1/";
 var openStatesKey = "&apikey=f58d2e11ccbe4471bdb7485c4fee0058";
