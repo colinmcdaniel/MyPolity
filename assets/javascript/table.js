3e88a9f7081b82689f9a17e341c862eb2307b2be
Spencer Charest <spencercharest@Spencers-MacBook-Pro.local>
2016-08-24 11:29:25 -0700

    on click works

--- assets/javascript/table.js
+++ assets/javascript/table.js
@@ -39,11 +39,28 @@ function drawRep(representative){
   }
 }
 function repInfo(representative){
+  $('#rep-office').empty();
+  $('#rep-phone').empty();
+  $('#rep-email').empty();
+  $('#rep-website').empty();
   var rep;
   for(var i = 0; i < Representitives.length; i++){
     if(representative == Representitives[i].name){
       rep = Representitives[i];
     }
+  }
+  $('#rep-name').text(rep.name);
+  for(var k = 0; k < rep.addresses.length; k++){
+    $('#rep-office').append('<h4>' + rep.addresses[k].replace(/\b[a-z]/g,function(f){return f.toUpperCase();}) + '</h4>');
+  }
+  for(var j = 0; j < rep.phones.length; j++){
+    $('#rep-phone').append('<h4><a href="tel:' + rep.phones[j] + '">' + rep.phones[j] + '</a></h4>');
+  }
+  for(var l = 0; l < rep.emails.length; l++){
+    $('#rep-email').append('<h4><a href="mailto:' + rep.emails[l] + '">' + rep.emails[l] + '</a></h4>');
+  }
+  for(var m = 0; m < rep.urls.length; m++){
+    $('#rep-website').append('<h4><a href="' + rep.urls[m] + '">' + rep.urls[m] + '</a></h4>');
   }
 }
 
